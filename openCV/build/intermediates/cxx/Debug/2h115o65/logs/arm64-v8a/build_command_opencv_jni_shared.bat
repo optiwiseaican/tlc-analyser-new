125df9bda710f83f18d0453bbf47486e21c0e2a8
Naman2701B <namanbadlani27@gmail.com>
2025-02-27 15:31:07 +0530

    nothing

---
+++ openCV/build/intermediates/cxx/Debug/2h115o65/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\Lenovo\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\Lenovo\\Desktop\\tlc-analyser-new\\openCV\\.cxx\\Debug\\2h115o65\\arm64-v8a" ^
+  opencv_jni_shared
